9aa9b64d0bcb612241849c8399b89078407c4bb8
zxhxw <415266418@qq.com>
2020-01-21 19:36:19 +0800

    新文件：   chess.py

---
+++ chess.py
@@ -0,0 +1,18 @@
+
+
+class chess(object):
+    def __init__(self):
+        self._name = ''
+        self._race = []  # maybe muilt race at the same time
+        self._occupation = []
+        self._warcry = []
+        self._deadtalk = []
+        self._attributes = []
+        self._cur_life = 0
+        self._life = 0
+        self._cur_attack_power = 0
+        self._attack_power = 0
+    def attack(other_chess):
+        # 攻击规则
+        
+        
